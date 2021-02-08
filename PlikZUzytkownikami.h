285c881a9b7069e7e3bcfb9db27f7006fd077ddc
Paulina Jarosz <paulinajarosz23@gmail.com>
2021-02-08 22:49:23 +0100

    Divide KsiazkaAdresowa to smaller class

---
+++ PlikZUzytkownikami.h
@@ -0,0 +1,27 @@
+#ifndef PLIKZUZYTKOWNIAKMI_H
+#define PLIKZUZYTKOWNIAKMI_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "Uzytkownik.h"
+#include "MetodyPomocnicze.h"
+
+using namespace std;
+
+class PlikZUzytkownikami {
+
+    string nazwaPlikuZUzytkownikami;
+    fstream plikTekstowy;
+
+    bool czyPlikJestPusty();
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+
+public:
+    PlikZUzytkownikami();
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+
+};
+
+#endif
