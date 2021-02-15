3e4a515154ffd762bb9c8a39f823316f06dd9573
Paulina Jarosz <paulinajarosz23@gmail.com>
2021-02-15 20:38:01 +0100

    Added setup filename at begining

--- PlikZUzytkownikami.cpp
+++ PlikZUzytkownikami.cpp
@@ -1,11 +1,8 @@
 #include "PlikZUzytkownikami.h"
 
-PlikZUzytkownikami :: PlikZUzytkownikami(){
-    nazwaPlikuZUzytkownikami = "Uzytkownicy.txt";
-}
-
 void PlikZUzytkownikami :: dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik)
 {
+    fstream plikTekstowy;
     string liniaZDanymiUzytkownika = "";
     plikTekstowy.open(nazwaPlikuZUzytkownikami.c_str(), ios::app);
 
@@ -13,7 +10,7 @@ void PlikZUzytkownikami :: dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik)
     {
         liniaZDanymiUzytkownika = zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(uzytkownik);
 
-        if (czyPlikJestPusty() == true)
+        if (czyPlikJestPusty(plikTekstowy) == true)
         {
             plikTekstowy << liniaZDanymiUzytkownika;
         }
@@ -27,7 +24,7 @@ void PlikZUzytkownikami :: dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik)
     plikTekstowy.close();
 }
 
-bool PlikZUzytkownikami :: czyPlikJestPusty()
+bool PlikZUzytkownikami :: czyPlikJestPusty(fstream& plikTekstowy)
 {
     plikTekstowy.seekg(0, ios::end);
     if (plikTekstowy.tellg() == 0)
@@ -50,6 +47,7 @@ string PlikZUzytkownikami :: zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionow
 vector <Uzytkownik> PlikZUzytkownikami :: wczytajUzytkownikowZPliku()
 {
     Uzytkownik uzytkownik;
+    fstream plikTekstowy;
     vector <Uzytkownik> uzytkownicy;
     string daneJednegoUzytkownikaOddzielonePionowymiKreskami = "";
 
